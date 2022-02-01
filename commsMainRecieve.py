92b59d42311a8548dbbeec670d44905321688a8e
AlexDom513 <57962269+AlexDom513@users.noreply.github.com>
2022-02-01 17:40:14 -0600

    fix

--- commsMainRecieve.py
+++ commsMainRecieve.py
@@ -6,6 +6,7 @@ UDP_Port = 5005                                             #specifies unused po
 LOCAL_IP = '0.0.0.0'                                        #specifies IPv4 addresses on the local machine
 UDP_IP = '192.168.1.144'                                    #target IP address (RasPi)
 
-t = threading.Thread(target=reciever.recv_msg)              #specifies that the additional thread will handle recieving messages
+recieve = reciever(UDP_Port, LOCAL_IP, UDP_IP)
+t = threading.Thread(target=recieve.recv_msg)              #specifies that the additional thread will handle recieving messages
 t.start()                                                   #start the additional thread
 reciever.send_msg()                                         #start the message sending system on the current thread
