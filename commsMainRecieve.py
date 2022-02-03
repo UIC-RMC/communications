d676b0b87ad5b23f333eadb3217bad1b9ca41906
AlexDom513 <57962269+AlexDom513@users.noreply.github.com>
2022-02-03 16:37:04 -0600

    test-ready

--- commsMainRecieve.py
+++ commsMainRecieve.py
@@ -1,11 +1,16 @@
 from udpModule import reciever
+from PyRoboteq import RoboteqHandler
+from PyRoboteq import roboteq_commands as cmds
 import threading
 import time
+
+controller = RoboteqHandler(debug_mode=True, exit_on_interrupt=False)
+connected = controller.connect("/dev/ttyACM0") # Insert your COM port (for windows) or /dev/tty{your_port} (Commonly /dev/ttyACM0) for linux.
 
 #Connection Parameters
 UDP_Port = 5005                                             #specifies unused port on network
 LOCAL_IP = '0.0.0.0'                                        #specifies IPv4 addresses on the local machine
-UDP_IP = '192.168.1.162'                                    #target IP address (RasPi)
+UDP_IP = '192.168.1.162'                                    #target IP address (external)
 
 recieve = reciever(UDP_Port, LOCAL_IP, UDP_IP)
 t = threading.Thread(target=recieve.recv_msg)              #specifies that the additional thread will handle recieving messages
@@ -13,6 +18,27 @@ t.start()                                                   #start the additiona
 
 t2 = threading.Thread(target=recieve.send_msg)
 t2.start()
+
+def control(x,y):
+
+    if x<300 and x>-300 and y<300 and y>-300:
+        controller.dual_motor_control(0, 0)
+        print("Stop")
+    if y>=300 and y<=1000 and x>-300 and x<300:
+        controller.dual_motor_control(400, 400)
+        print("forward/n")
+    elif y<=-300 and y>=-1000 and x>-300 and x<300:
+        controller.dual_motor_control(400, 400)
+        print("reverse/n")
+    elif x>=300 and x<=1000 and y>-300 and y<300:
+        controller.dual_motor_control(400, -400)
+        print("right/n")
+    elif x<=-300 and x>=-1000 and y>-300 and y<300:
+        controller.dual_motor_control(-400, 400)
+        print("left/n")
+    else:
+        controller.dual_motor_control(0, 0)
+        print("Stop")
 
 while True:
     a = (recieve.msg.split(' '))
@@ -22,7 +48,11 @@ while True:
         y = -1 * int(float(a[1]) * 1000)
         #print(str(x) + ' ' + str(y))
         #print(x)
-        print(y)
+        #print(y)
+        control(x,y)
+
+
+
     except:
        pass
     time.sleep(.25)
