054e4a975fcf8104576fad62c397975fa3fc4921
AlexDom513 <57962269+AlexDom513@users.noreply.github.com>
2022-03-01 00:51:58 -0600

    fix

--- Prototyping/tcp-protoBuf/protoBuf-reciever.py
+++ Prototyping/tcp-protoBuf/protoBuf-reciever.py
@@ -1,3 +1,4 @@
+'''
 import socket
 
 HOST = socket.gethostname()
@@ -9,3 +10,18 @@ s.connect((HOST, PORT))
 while True:
     msg = s.recv(1024)
     print(msg.decode('utf-8'))
+'''
+
+# echo-client.py
+
+import socket
+
+HOST = "127.0.0.1"  # The server's hostname or IP address
+PORT = 65432  # The port used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b"Hello, world")
+    data = s.recv(1024)
+
+print(f"Received {data!r}")
--- Prototyping/tcp-protoBuf/protoBuf-transmitter.py
+++ Prototyping/tcp-protoBuf/protoBuf-transmitter.py
@@ -1,4 +1,5 @@
-#https://www.youtube.com/watch?v=Lbfe3-v7yE0
+
+'''#https://www.youtube.com/watch?v=Lbfe3-v7yE0
 import socket
 import time
 
@@ -15,3 +16,23 @@ while True:
     clientsocket.send(bytes('Hello World!','utf-8'))
     time.sleep(2)
     print('active')
+'''
+
+# echo-server.py
+
+import socket
+
+HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print(f"Connected by {addr}")
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
