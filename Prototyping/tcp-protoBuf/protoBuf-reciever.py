14c6dedeef39003d8d00d07e8189db7c03a3e23b
AlexDom513 <57962269+AlexDom513@users.noreply.github.com>
2022-03-01 00:47:20 -0600

    fix

--- Prototyping/tcp-protoBuf/protoBuf-reciever.py
+++ Prototyping/tcp-protoBuf/protoBuf-reciever.py
@@ -1,6 +1,6 @@
 import socket
 
-HOST = '0.0.0.0'
+HOST = socket.gethostname()
 PORT = 5005
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
--- Prototyping/tcp-protoBuf/protoBuf-transmitter.py
+++ Prototyping/tcp-protoBuf/protoBuf-transmitter.py
@@ -2,7 +2,7 @@
 import socket
 import time
 
-HOST = '0.0.0.0'
+HOST = socket.gethostname()
 PORT = 5005
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
