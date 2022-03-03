291bc5414474ac04888371e29c977b5f7c5c046a
AlexDom513 <57962269+AlexDom513@users.noreply.github.com>
2022-03-03 17:31:02 -0600

    update

--- Prototyping/tcp-protoBuf/protoBuf-reciever.py
+++ Prototyping/tcp-protoBuf/protoBuf-reciever.py
@@ -1,39 +1,13 @@
-'''
 import socket
-
-HOST = socket.gethostname()
-PORT = 5005
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
-
-while True:
-    msg = s.recv(1024)
-    print(msg.decode('utf-8'))
-'''
-'''
-# echo-client.py
-
-import socket
-
-HOST = "127.0.0.1"  # The server's hostname or IP address
-PORT = 65432  # The port used by the server
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b"Hello, world")
-    data = s.recv(1024)
-    print(f"Received {data!r}")
-'''
-
-import socket
+import test_pb2 as pb2
 
 host = "192.168.1.162"
-port = 61626        #this number does not matter just needs to be the same across server and client
-
+port = 61626
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host, port))
+
 s.sendall(b'Hello, world')
-data = s.recv(1024)
+while True:
+    data = s.recv(1024)
+    print(repr(data))
 s.close()
-print(repr(data))
--- Prototyping/tcp-protoBuf/protoBuf-transmitter.py
+++ Prototyping/tcp-protoBuf/protoBuf-transmitter.py
@@ -1,33 +1,6 @@
 
-#https://www.youtube.com/watch?v=Lbfe3-v7yE0
-#https://notenoughtech.com/raspberry-pi/rpi-socket-protocol/
-'''
 import socket
-
-HOST = '' # Server IP or Hostname
-PORT = 12345 # Pick an open Port (1000+ recommended), must match the client sport
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print('Socket created')
-
-#managing error exception
-try:
-	s.bind((HOST, PORT))
-	s.listen(5)
-	print('Socket awaiting messages')
-	(conn, addr) = s.accept()
-	print('Connected')
-except:
-        print('fail bind')
-
-# awaiting for message
-while True:
-	data = conn.recv(1024)
-	print('I sent a message back in response to: ' + data)
-	reply = ''
-	conn.close() # Close connections
-	'''
-
-import socket
+import test_pb2 as pb2
 
 host = ''
 port = 61626
