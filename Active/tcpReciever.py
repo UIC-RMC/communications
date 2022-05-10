5b883e7eb827461fdd0c66fe841764043d3371eb
AlexDom513 <57962269+AlexDom513@users.noreply.github.com>
2022-05-10 13:44:21 -0500

    temp

--- Active/tcpReciever.py
+++ Active/tcpReciever.py
@@ -1,7 +1,44 @@
 import serdes
 import tcpModule
 import RPi.GPIO as GPIO
+import os
 
+
+while True:
+    try:
+        HOST = '192.168.1.162'
+        PORT = 2356
+
+        #monitor pi with GPIO
+        LED_PIN = 17
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(LED_PIN, GPIO.OUT)
+        GPIO.output(LED_PIN, GPIO.HIGH)
+
+        #create serializer and reciever objects
+        ser = serdes.deserializer()
+        print('Waiting to connect!')
+        recv = tcpModule.reciever(HOST, PORT)
+
+        #recieve messages
+        while True:
+            msg = recv.recieve_msg()
+            data = ser.decode(msg) #note that data is a python list that contains the different values
+            print(data[0]) #contains the message type
+
+            #do the stuff based on the data
+            if data[0] == 'move':
+                pass
+
+            elif data[0] == 'mine':
+                pass
+
+            elif data[0] == 'dump':
+                pass
+        
+    except:
+        os.system('tcpReciever.py')
+'''
 HOST = '192.168.1.162'
 PORT = 2356
 
@@ -31,3 +68,4 @@ while True:
 
     elif data[0] == 'dump':
         pass
+'''
